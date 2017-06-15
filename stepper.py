46a78cef93f0031563f5ae34c5a9b9dfed467b2f
Laurencio Alan García <laurencio.alan@gmail.com>
2017-06-14 19:18:19 -0500

    Prueba con secuencia de 7 pasos

--- stepper.py
+++ stepper.py
@@ -1,34 +1,40 @@
 import RPi.GPIO as GPIO
 import time
-   
+
 GPIO.setmode(GPIO.BCM)
-     
-enable_pin = 18
-coil_A_1_pin = 4 #ROSA
-coil_A_2_pin = 24 #AMARILLO
-coil_B_1_pin = 17 # NARANJA
-coil_B_2_pin = 23 # AZUL
-     
+
+enable_pin = 18 # Enable H Bridge
+coil_A_1_pin = 4 #Orange
+coil_B_1_pin = 17 # Pink
+coil_A_2_pin = 24 #Yellow
+coil_B_2_pin = 23 # Blue
+
 GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
-     
+
 GPIO.output(enable_pin, 1)
-     
-def forward(delay, steps):  
+
+def forward(delay, steps):
     for i in range(0, steps):
-        setStep(1, 0, 1, 0)
-        time.sleep(delay)
         setStep(1, 0, 0, 1)
         time.sleep(delay)
-        setStep(0, 1, 0, 1)
+        setStep(1, 0, 0, 0)
+        time.sleep(delay)
+        setStep(0, 1, 0, 0)
         time.sleep(delay)
         setStep(0,1, 1, 0)
         time.sleep(delay)
-     
-def backwards(delay, steps):  
+        setStep(0,0, 1, 0)
+        time.sleep(delay)
+        setStep(0,0, 1, 1)
+        time.sleep(delay)
+        setStep(0,0, 0, 1)
+        time.sleep(delay)
+
+def backwards(delay, steps):
     for i in range(0, steps):
         setStep(0, 1, 1, 0)
         time.sleep(delay)
@@ -38,17 +44,16 @@ def backwards(delay, steps):
         time.sleep(delay)
         setStep(1, 0, 1, 0)
         time.sleep(delay)
-     
-      
+
 def setStep(w1, w2, w3, w4):
     GPIO.output(coil_A_1_pin, w1)
-    GPIO.output(coil_A_2_pin, w2)
-    GPIO.output(coil_B_1_pin, w3)
+    GPIO.output(coil_B_1_pin, w2)
+    GPIO.output(coil_A_2_pin, w3)
     GPIO.output(coil_B_2_pin, w4)
-     
+
 while True:
     delay = raw_input("Delay between steps (milliseconds)?")
     steps = raw_input("How many steps forward? ")
     forward(int(delay) / 1000.0, int(steps))
-    steps = raw_input("How many steps backwards? ")
-    backwards(int(delay) / 1000.0, int(steps))   
+    #steps = raw_input("How many steps backwards? ")
+    #backwards(int(delay) / 1000.0, int(steps))
