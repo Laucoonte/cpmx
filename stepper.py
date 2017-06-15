09987156266abb077330c84c0f50fa44fd3eeb9f
Laurencio Alan García <laurencio.alan@gmail.com>
2017-06-14 19:22:40 -0500

    Ahora sí 8 pasos

--- stepper.py
+++ stepper.py
@@ -23,17 +23,18 @@ def forward(delay, steps):
         time.sleep(delay)
         setStep(1, 0, 0, 0)
         time.sleep(delay)
-        setStep(0, 1, 0, 0)
+        setStep(1, 1, 0, 0)
+        time.sleep(delay)
+        setStep(0,1, 0, 0)
         time.sleep(delay)
         setStep(0,1, 1, 0)
         time.sleep(delay)
         setStep(0,0, 1, 0)
         time.sleep(delay)
-        setStep(0,0, 1, 1)
+        setStep(0, 0, 1, 1)
         time.sleep(delay)
-        setStep(0,0, 0, 1)
+        setStep(0, 0, 0, 1)
         time.sleep(delay)
-
 def backwards(delay, steps):
     for i in range(0, steps):
         setStep(0, 1, 1, 0)
