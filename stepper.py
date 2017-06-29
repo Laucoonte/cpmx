a9ade55aead9bcdd7d53a54ae25bbdd39bbb1938
Laurencio Alan García <laurencio.alan@gmail.com>
2017-06-29 18:46:14 -0500

    Library for stepper >:) by Roland

--- stepper.py
+++ stepper.py
@@ -1,66 +1,132 @@
-import RPi.GPIO as GPIO
-import time
-import picamera
-# Load Camera
+# ____________________________________________________
+# ____________________________________________________
+""" import piCamera
+# load Camera
 camera=piCamera()
 camera.resolution (320,240)
-camera.capture("olakease.jpg")
+camera.capture("olakease.jpg") """
+# ____________________________________________________
+# SETUP
 
-#Set mode for GPIO
+# Importacion de bibliotecas
+import RPi.GPIO as GPIO # Board de Raspberry Pi
+import time # "Control" de tiempo
+
+# Numeracion de pines por el sistema GPIO
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(0)
 
-enable_pin = 18 # Enable H Bridge
-coil_A_1_pin = 4 #Orange
-coil_B_1_pin = 17 # Pink
-coil_A_2_pin = 24 #Yellow
-coil_B_2_pin = 23 # Blue
-# Fuck you Alan
-GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
+# Asignacion de pines de control del motor a pasos
+A1 = 26
+B1 = 19
+A2 = 13
+B2 = 6
 
-GPIO.output(enable_pin, 1)
+# Configuracion del modo de trabajo de los pines
+GPIO.setup(A1,GPIO.OUT)
+GPIO.setup(B1,GPIO.OUT)
+GPIO.setup(A2,GPIO.OUT)
+GPIO.setup(B2,GPIO.OUT)
 
-def forward(delay, steps):
-    for i in range(0, steps):
-        setStep(1, 0, 0, 1)
-        time.sleep(delay)
-        setStep(1, 0, 0, 0)
-        time.sleep(delay)
-        setStep(1, 1, 0, 0)
-        time.sleep(delay)
-        setStep(0,1, 0, 0)
-        time.sleep(delay)
-        setStep(0,1, 1, 0)
-        time.sleep(delay)
-        setStep(0,0, 1, 0)
-        time.sleep(delay)
-        setStep(0, 0, 1, 1)
-        time.sleep(delay)
-        setStep(0, 0, 0, 1)
-        time.sleep(delay)
-def backwards(delay, steps):
-    for i in range(0, steps):
-        setStep(0, 1, 1, 0)
-        time.sleep(delay)
-        setStep(0, 1, 0, 1)
-        time.sleep(delay)
-        setStep(1, 0, 0,1)
-        time.sleep(delay)
-        setStep(1, 0, 1, 0)
-        time.sleep(delay)
+# Asignacion de velocidad del motor a pasos (seg)
+esp = 0.001 # Correcto
+# ____________________________________________________
+# FUNCIONES
+# *
+# CONFIGURACION:
+# Funcion de asignacion de senial de salidas
+def setStp(stp1,stp2,stp3,stp4):
+    GPIO.output(A1,stp1)
+    GPIO.output(B1,stp2)
+    GPIO.output(A2,stp3)
+    GPIO.output(B2,stp4)
+#*
+# ADELANTE:
+# Funcion de movimiento hacia adelante del motor
+def adl(stp): # Recibe numero de pasos
+    cnt = 0
+    for i in range(0,stp):
+        if(cnt == stp): #### 1
+            break
+        cnt = cnt + 1
+        setStp(1,0,1,0)
+        time.sleep(esp)
+        if(cnt == stp): #### 2
+            break
+        cnt = cnt + 1
+        setStp(0,1,1,0)
+        time.sleep(esp)
+        if(cnt == stp): #### 3
+            break
+        cnt = cnt + 1
+        setStp(0,1,0,1)
+        time.sleep(esp)
+        if(cnt == stp): #### 4
+            break
+        cnt = cnt + 1
+        setStp(1,0,0,1)
+        time.sleep(esp)
 
-def setStep(w1, w2, w3, w4):
-    GPIO.output(coil_A_1_pin, w1)
-    GPIO.output(coil_B_1_pin, w2)
-    GPIO.output(coil_A_2_pin, w3)
-    GPIO.output(coil_B_2_pin, w4)
+# *
+# ATRAS:
+# Funcion de movimiento hacia atras del motor
+def atr(stp): # Recibe numero de pasos
+    cnt = 0
+    for i in range(0,stp):
+        if(cnt == stp): #### 1
+            break
+        cnt = cnt + 1
+        setStp(1,0,0,1)
+        time.sleep(esp)
+        if(cnt == stp): #### 2
+            break
+        cnt = cnt + 1
+        setStp(0,1,0,1)
+        time.sleep(esp)
+        if(cnt == stp): #### 3
+            break
+        cnt = cnt + 1
+        setStp(0,1,1,0)
+        time.sleep(esp)
+        if(cnt == stp): #### 4
+            break
+        cnt = cnt + 1
+        setStp(1,0,1,0)
+        time.sleep(esp)
 
-while True:
-    delay = raw_input("Delay between steps (milliseconds)?")
-    #steps = raw_input("How many steps forward? ")
-    #forward(int(delay) / 1000.0, int(steps))
-    steps = raw_input("How many steps backwards? ")
-    backwards(int(delay) / 1000.0, int(steps))
+#*
+# DESPLAZAMIENTO:
+# Mueve la camara a la posicion deseada
+def dsp(pscInc,pscFin): # Requiere posicion inicial y final
+    stp = pscFin - pscInc
+    if(pscFin < 0):
+        time.sleep(0.5)
+        setStp(0,0,0,0)
+        return pscInc
+#   #
+    if(stp >= 0):
+        adl(stp*2)
+        time.sleep(0.5)
+        setStp(0,0,0,0)
+        return pscFin
+    else:
+        stp = stp * -1
+        atr(stp*2)
+        time.sleep(0.5)
+        setStp(0,0,0,0)
+        return pscFin
+
+#*
+# ____________________________________________________
+
+# LOOP
+#
+"""while True:
+    time.sleep(0.5)
+    setStp(0,0,0,0)
+    pscFin = raw_input("Posicion de destino: ") # 0 - 480
+    pscFin = int(pscFin)
+    pscInc = dsp(pscInc,pscFin)
+    print "Ya estas peinado pa tras!"
+
+"""
